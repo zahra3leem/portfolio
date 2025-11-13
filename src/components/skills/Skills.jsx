9813ccb8a46998c92ce23362b870ae007b260513
Developer <developer@company.com>
2025-11-13 19:26:55 +0200

    Fix Skills component: update image imports and file paths

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,6 +1,6 @@
-import Frontend from "../../assets/frontend.png";
-import Frontend_1 from "../../assets/frontend_1.png";
-import Frontend_2 from "../../assets/frontend_2.png";
+import ff from "../../assets/ff.png";
+import fff from "../../assets/fff.png";
+import ffff from "../../assets/ffff.png";
 
 export default function Skills() {
   return (
@@ -29,19 +29,19 @@ export default function Skills() {
             className="grid gap-4 col-start-1 col-end-3 row-start-1 sm:mb-6 sm:grid-cols-4 lg:gap-6 lg:col-start-2 lg:row-end-6 lg:row-span-6 lg:mb-0"
           >
             <img
-              src={Frontend}
+              src={ff}
               alt=""
               className="w-full h-60 object-cover rounded-lg sm:h-52 sm:col-span-2 lg:col-span-full"
               loading="lazy"
             />
             <img
-              src={Frontend_1}
+              src={fff}
               alt=""
               className="hidden w-full h-52 object-cover rounded-lg sm:block sm:col-span-2 md:col-span-1 lg:row-start-2 lg:col-span-2 lg:h-32"
               loading="lazy"
             />
             <img
-              src={Frontend_2}
+              src={ffff}
               alt=""
               className="hidden w-full h-52  rounded-lg md:block lg:row-start-2 lg:col-span-2 lg:h-32"
               loading="lazy"
